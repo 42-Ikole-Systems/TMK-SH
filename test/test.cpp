fd47d1b97e7b8f4537afdb51361bbbc688eccce0
Tishj <t_b@live.nl>
2023-10-08 14:03:30 +0200

    add catch2 library + basic test

---
+++ test/test.cpp
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "shell/stdin_reader.hpp"
+#include "shell/parser.hpp"
+#include "shell/util.hpp"
+#include "shell/lexer/lexer.hpp"
+#include "shell/lexer/line_char_provider.hpp"
+#include <utility>
+#include "shell/print.hpp"
+#include "shell/executor/executor.hpp"
+
+using namespace shell;
+
+TEST_CASE("basic shell", "[shell]") {
+	string line = "/bin/ls -la";
+
+	line.push_back('\n');
+	auto chars = LineCharProvider(line);
+	auto lexer = Lexer(chars);
+	auto parser = Parser(lexer);
+	Ast ast = parser.getNextCommand();
+	ast.print();
+}
