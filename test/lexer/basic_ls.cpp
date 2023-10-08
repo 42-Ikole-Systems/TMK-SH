745e2b37ea5e5f5a77c6079ce39b18555c4f247a
Tishj <t_b@live.nl>
2023-10-08 19:58:04 +0200

    add tests for less/greater operator

--- test/lexer/basic_ls.cpp
+++ test/lexer/basic_ls.cpp
@@ -11,3 +11,25 @@ TEST_CASE("ls -la", "[lexer]") {
 	};
 	assertExpectedTokens(line, expected);
 }
+
+TEST_CASE("greater token", "[lexer]") {
+	vector<Token> expected = {
+		Token(Token::Type::Word, WordToken{"/bin/ls"}),
+		Token(Token::Type::Great, OperatorToken()),
+		Token(Token::Type::Word, WordToken{"bla"}),
+		Token(Token::Type::Newline, Newline())
+	};
+	assertExpectedTokens("/bin/ls > bla", expected);
+	assertExpectedTokens("/bin/ls >bla", expected);
+}
+
+TEST_CASE("less token", "[lexer]") {
+	vector<Token> expected = {
+		Token(Token::Type::Word, WordToken{"/bin/ls"}),
+		Token(Token::Type::Less, OperatorToken()),
+		Token(Token::Type::Word, WordToken{"bla"}),
+		Token(Token::Type::Newline, Newline())
+	};
+	assertExpectedTokens("/bin/ls < bla", expected);
+	assertExpectedTokens("/bin/ls <bla", expected);
+}
