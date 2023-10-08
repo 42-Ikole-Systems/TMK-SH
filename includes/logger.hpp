5c79e4e6b264f05e3f34caf6fb3d01c9e8819392
Ingmar Kole <ikole@student.codam.nl>
2023-10-08 13:54:37 +0200

    feat: added buffered logger

--- includes/logger.hpp
+++ includes/logger.hpp
@@ -3,14 +3,16 @@
 #include "settings.hpp"
 #include "print.hpp"
 
-#define LOG_INFO(format, ...)  shell::Logger::getInstance().write(shell::Logger::LogLevel::Info, format, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) shell::Logger::getInstance().write(shell::Logger::LogLevel::Error, format, ##__VA_ARGS__)
+#include <sstream>
+
+#define LOG_INFO(format, ...)  shell::Logger::getInstance().write(shell::LogLevel::Info, format, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) shell::Logger::getInstance().write(shell::LogLevel::Error, format, ##__VA_ARGS__)
 #define LOG_WARNING(format, ...)                                                                                       \
-	shell::Logger::getInstance().write(shell::Logger::LogLevel::Warning, format, ##__VA_ARGS__)
+	shell::Logger::getInstance().write(shell::LogLevel::Warning, format, ##__VA_ARGS__)
 
 #ifdef DEBUG
 // todo only print `[DEBUG]` at start of line (i.e. handle flushing)
-#define LOG_DEBUG(format, ...) shell::Logger::getInstance().write(shell::Logger::LogLevel::Debug, format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) shell::Logger::getInstance().write(shell::LogLevel::Debug, format, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(format, ...)
 #endif
@@ -18,39 +20,51 @@
 namespace shell {
 
 /*!
- * @brief Manages Logging.
+ * @brief -.
+*/
+enum class LogLevel { Info, Warning, Error, Debug };
+
+#ifdef DEBUG
+	constexpr auto g_logLevel = LogLevel::Debug;
+#else
+	constexpr auto g_logLevel = LogLevel::Error;
+#endif
+
+constexpr auto GetLogLevelPrefix(LogLevel logLevel)
+{
+	switch (logLevel) {
+		case LogLevel::Info:
+			return "[Info]: ";
+		case LogLevel::Warning:
+			return "[Warning]: ";
+		case LogLevel::Error:
+			return "[Error]: ";
+		case LogLevel::Debug:
+			return "[Debug]: ";
+	};
+}
+
+/*!
+ * @brief Singleton Logging class.
  */
 class Logger {
+
+	/*!
+	 * @brief -.
+	*/
 	Logger() = default;
 
 public:
-	/*!
-	 * @brief -.
-	 */
-	enum class LogLevel { Info, Warning, Error, Debug };
 
 	/*!
-	 * @brief Writes message like printf but with loglevel prefix.
+	 * @brief Writes message like printf but with loglevel prefix (use like tprintf).
 	 * @param logLevel
 	 * @param format
 	 * @param args
 	 */
 	template <class... Args>
 	void write(LogLevel logLevel, const char *format, Args &&...args) {
-		switch (logLevel) {
-			case LogLevel::Info:
-				tprintf("[Info]: ");
-				break;
-			case LogLevel::Warning:
-				tprintf("[Warning]: ");
-				break;
-			case LogLevel::Error:
-				tprintf("[Error]: ");
-				break;
-			case LogLevel::Debug:
-				tprintf("[Debug]: ");
-				break;
-		};
+		tprintf(GetLogLevelPrefix(logLevel));
 		tprintf(format, std::forward<Args>(args)...);
 	}
 
@@ -62,6 +76,61 @@ public:
 		static Logger logger;
 		return logger;
 	}
+};
+
+/*!
+ * @brief Buffered logging class
+*/
+class BLogger
+{
+	const LogLevel logLevel; /*!< -. */
+	std::stringstream buffer; /*!< -. */
+
+public:
+
+	/*!
+	 * @brief Buffer will be flushed on destruction of object.
+	 * 		  Nothing will be written when the global loglevel is lower than the loglevel you try to write with.
+	 * @param logLevel
+	*/
+	BLogger(LogLevel logLevel_) : logLevel(logLevel_)
+	{
+		if (logLevel <= g_logLevel)
+		{
+			buffer << GetLogLevelPrefix(logLevel);
+		}
+	}
+
+	/*!
+	 * @brief .-
+	*/
+	~BLogger()
+	{
+		flush();
+	}
+
+	/*!
+	 * @brief Adds value to buffer.
+	*/
+	template<class T>
+	BLogger& operator << (const T& value)
+	{
+		if (logLevel <= g_logLevel)
+		{
+			buffer << value;
+		}
+		return *this;
+	}
+
+	/*!
+	 * @brief flushes the buffer.
+	*/
+	void flush()
+	{
+		std::cout << buffer.str();
+		buffer.clear();
+	}
+
 };
 
 } // namespace shell
